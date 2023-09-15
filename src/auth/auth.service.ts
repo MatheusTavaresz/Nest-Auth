6dc8f3b1d736d502bf04245d3ab80d09cfd203b9
Matheus Patrick <mathtg2@gmail.com>
2023-09-14 21:54:22 -0300

    Add token and user validation methods to service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from 'src/users/models/users.model';
+import { sign } from 'jsonwebtoken';
+import { Request } from 'express';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @InjectModel('User')
+        private readonly usersModel: Model<User>,
+    ){}
+
+    public async createAcessToken(userId: string): Promise<string>{
+        return sign({userId}, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXPIRATION
+        })
+    }
+
+    public async validateUser(userId: string): Promise<User>{
+        const user = await this.usersModel.findOne({_id: userId});
+
+        if(!user){
+            throw new UnauthorizedException('User not found.');
+        } 
+        
+        return user
+    }
+
+    private jwtExtractor(request: Request): string{
+        const authHeader = request.headers.authorization;
+
+        if(!authHeader){
+            throw new BadRequestException('Bad request');
+        }
+
+        const [, token] = authHeader.split(' ');
+        return token
+    }
+
+    public returnJwtExtractor(): (request: Request) => string{
+        return this.jwtExtractor;
+    }
+}
